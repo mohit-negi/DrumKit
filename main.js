6b1e3ca2d4815535045efacaeb2f355d1d048001
cpt arduous <mohitneg377@gmail.com>
2022-07-25 09:01:48 +0530

    JS drumkit done

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 window.addEventListener('keydown',(e)=>{
-    console.log(e);
+    const aud = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!aud) return;    //if it isn't one of the chosen one it(func) will stop
+    aud.currentTime = 0; //it'll rewind the audio
+    aud.play();
+
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    key.classList.add('playing');
+
+    function removeTransition(e){
+        if(e.propertyName != 'transform') return;
+        this.classList.remove('playing');
+
+    }
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(element=>{
+        element.addEventListener('transitionend',removeTransition)
+    });
 });
