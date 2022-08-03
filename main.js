86fbdfc04c9670617b3cacb9dc154bc656e8a77c
cpt arduous <mohitneg377@gmail.com>
2022-08-03 10:59:58 +0530

    Added touch/click support to the buttons

--- main.js
+++ main.js
@@ -10,10 +10,34 @@ window.addEventListener('keydown',(e)=>{
     function removeTransition(e){
         if(e.propertyName != 'transform') return;
         this.classList.remove('playing');
-
     }
     const keys = document.querySelectorAll('.key');
     keys.forEach(element=>{
-        element.addEventListener('transitionend',removeTransition)
+        element.addEventListener('transitionend',removeTransition);
     });
 });
+
+clicker();
+function clicker()
+{
+    let keyClicked = document.querySelectorAll('.key');
+
+    for(let i=0;i<keyClicked.length;i++)
+    {
+        keyClicked[i].addEventListener('click',()=>{
+            const aud = document.querySelector(`audio[data-key="${keyClicked[i].id}"]`);
+            if(!aud) return;    //if it isn't one of the chosen one it(func) will stop
+            aud.currentTime = 0; //it'll rewind the audio
+            aud.play();
+
+            const key = document.querySelector(`div[data-key="${keyClicked[i].id}"]`);
+            key.classList.add('playing');
+
+            function removeTransition(e){
+                //if(e.propertyName != 'transform') return;
+                this.classList.remove('playing');
+            }
+            keyClicked[i].addEventListener('transitionend',removeTransition);
+        });
+    }
+}
